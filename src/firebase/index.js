d9365dbb7870251bd1311d60716d9020931c080e
Marcio Willian <willian_mw.cc@hotmail.com>
2020-07-12 17:08:40 -0300

    D - 0.1.1 - Configurando autenticação de usuário

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase/app'
-import 'firebase/storage'
+import 'firebase/auth'
 import 'firebase/storage'
 import 'firebase/database'
 
--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -1,15 +1,21 @@
 <template>
-  <form class="form-login">
+  <form @submit.prevent="doLogin()" class="form-login">
     <div class="card">
       <div class="card-header text-center">
         <h1 class="mb-0">Expenses</h1>
       </div>
       <div class="card-body">
         <div class="form-group">
-          <input type="text" class="form-control" placeholder="E-mail" />
+          <input required type="email" v-model="email" class="form-control" placeholder="E-mail" />
         </div>
         <div class="form-group">
-          <input type="password" class="form-control" placeholder="Senha" />
+          <input
+            required
+            type="password"
+            v-model="password"
+            class="form-control"
+            placeholder="Senha"
+          />
         </div>
         <button class="btn btn-primary btn-block">Entrar</button>
       </div>
@@ -18,7 +24,28 @@
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Login",
+  data: () => ({
+    email: "",
+    password: ""
+  }),
+  methods: {
+    async doLogin() {
+      const { email, password } = this
+
+      try {
+        const res = await this.$firebase
+          .auth()
+          .signInWithEmailAndPassword(email, password)
+
+        console.log(res)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
@@ -27,8 +54,11 @@ export default {};
   display: flex;
   align-items: center;
   justify-content: center;
+  h1 {
+    font-size: 18pt;
+  }
   .card {
-    width: 25%;
+    width: 30%;
     color: var(--dark);
   }
 }
