f349442694804e77d114e8c43427a7b895352707
Marcio Willian <willian_mw.cc@hotmail.com>
2020-07-17 23:00:28 -0300

    D - 0.3.0 - Customizando layout de cadastro de despesas

--- src/components/layout/LayoutNewExpenses.vue
+++ src/components/layout/LayoutNewExpenses.vue
@@ -5,34 +5,64 @@
       Novo gasto
     </button>
 
-    <div class="modal fade" :class="{show: showModal}" :style="{display: showModal ? 'block' : 'none'}" >
-      <div class="modal-dialog">
+    <form @submit.prevent="submit()">
+      <div
+      class="modal fade"
+      :class="{show: showModal}"
+      :style="{display: showModal ? 'block' : 'none'}"
+    >
+      <div class="modal-dialog modal-lg">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLiveLabel">Modal title</h5>
+            <h5 class="modal-title" id="exampleModalLiveLabel">Adicionar novo gasto</h5>
             <button type="button" class="close" @click="closeModal()" aria-label="Close">
               <span aria-hidden="true">×</span>
             </button>
           </div>
           <div class="modal-body">
-            <p>Woohoo, you're reading this text in a modal!</p>
+            <div class="row">
+              <div class="form-group col-8">
+                <label for="">Descrição:</label>
+                <input v-model="form.description" type="text" class="form-control" required>
+              </div>
+              <div class="form-group col-4">
+                <label for="">Valor (R$):</label>
+                <input v-model="form.value" type="text" class="form-control" required>
+              </div>
+            </div>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" @click="closeModal()">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
+            <button type="button" class="btn btn-secondary" @click="closeModal()">Fechar</button>
+            <button @click="submit()" class="btn btn-primary">
+              Incluir novo gasto
+              </button>
           </div>
         </div>
       </div>
     </div>
 
-    <div class="modal-backdrop fade" :style="{display: showModal ? 'block' : 'none'}" :class="{show: showModal}"></div>
+    </form>
+    <div
+      class="modal-backdrop fade"
+      :style="{display: showModal ? 'block' : 'none'}"
+      :class="{show: showModal}"
+    ></div>
   </div>
 </template>
 
 <script>
 export default {
-  data: () => ({ showModal: false }),
+  data: () => ({
+    showModal: false,
+    form: {
+      value: '',
+      description: ''
+    }
+    }),
   methods: {
+    submit () {
+      console.log('ok')
+    },
     closeModal() {
       this.showModal = false;
     }
@@ -41,4 +71,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.modal {
+  color: var(--darker);
+}
 </style>
